862360367ee54fbf11e74816461fe7e916c35f6a
tohacking <152132683+tohacking@users.noreply.github.com>
2024-09-11 13:40:12 -0700

    feat: contract ABIs

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,28 @@ export * from "./zkwasm";
 
 export * from "./gameplay_simulator";
 
-export * from "./spin_game/";
+export * from "./spin_game/prover_dummy";
+
+export * from "./spin_game/prover_zk";
+
+export * from "./spin_game/prover_opzk";
+
+export * from "./spin_game/spin_game";
+
+import SpinOPZKGameContractABI from "./contracts/game_opzk/GameContract.sol/SpinOPZKGameContract.json";
+
+import SpinZKGameContractABI from "./contracts/game_zk/GameContract.sol/SpinZKGameContract.json";
+
+import StakingContractABI from "./contracts/game_opzk/StakingContract.sol/StakingContract.json";
+
+import GameStateStorageABI from "./contracts/shared/GameStateStorage.sol/GameStateStorage.json";
+
+import MultiSenderABI from "./contracts/shared/MultiSender.sol/MultiSender.json";
+
+export {
+    SpinOPZKGameContractABI,
+    SpinZKGameContractABI,
+    StakingContractABI,
+    GameStateStorageABI,
+    MultiSenderABI,
+};
--- src/spin_game/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./prover_dummy";
-
-export * from "./prover_zk";
-
-export * from "./prover_opzk";
-
-export * from "./spin_game";
